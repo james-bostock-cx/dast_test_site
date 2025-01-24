b5f45a31f437985c4863489cef559ae6db5dbff2
James Bostock <James.Bostock@checkmarx.com>
2025-01-24 03:55:16 +0000

    onfigure the Flask app to listen on all network interfaces

--- run.ps1
+++ run.ps1
@@ -1,4 +1,5 @@
 $env:FLASK_APP = 'dast_test_site.py'
 $env:FLASK_DEBUG = 1
 $env:FLASK_ENV = 'development'
+$env:FLASK_RUN_HOST = '0.0.0.0'
 flask run
--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 export FLASK_APP='dast_test_site.py'
 export FLASK_DEBUG=1
 export FLASK_ENV='development'
+export FLASK_RUN_HOST=0.0.0.0
 flask run
