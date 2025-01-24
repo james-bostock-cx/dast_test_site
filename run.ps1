148c65cab506082d6d9e7beb9e352b8a199d2d5d
James Bostock <James.Bostock@checkmarx.com>
2025-01-24 03:52:45 +0000

    Rename Python file and update driver scripts accordingly

--- run.ps1
+++ run.ps1
@@ -1,4 +1,4 @@
-$env:FLASK_APP = 'flask_auth_app.py'
+$env:FLASK_APP = 'dast_test_site.py'
 $env:FLASK_DEBUG = 1
 $env:FLASK_ENV = 'development'
 flask run
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export FLASK_APP='flask_auth_app.py'
+export FLASK_APP='dast_test_site.py'
 export FLASK_DEBUG=1
 export FLASK_ENV='development'
 flask run
